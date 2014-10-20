8bbbc7fa468b1be3ef2f22245117bd58a10f9eef
Jade Freeman <jade.freeman.75@gmail.com>
2014-10-20 11:13:50 -0400

    code climate formatting changes (test)

--- src/AdvancedDraw/widget/FillStylePicker.js
+++ src/AdvancedDraw/widget/FillStylePicker.js
@@ -1,75 +1,76 @@
 define( [
-            'dojo/_base/declare',
-            'dojo/_base/lang',
-            'dijit/_WidgetBase',
-            'dijit/_TemplatedMixin',
-            'dijit/_WidgetsInTemplateMixin',
-            'dojo/text!./templates/FillStylePicker.html',
-            'dojo/i18n!../nls/resource',
-            'dijit/form/Select',
-            'xstyle/css!./css/FillStylePicker.css'
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin',
+    'dijit/_WidgetsInTemplateMixin',
+    'dojo/text!./templates/FillStylePicker.html',
+    'dojo/i18n!../nls/resource',
+    'dijit/form/Select',
+    'xstyle/css!./css/FillStylePicker.css'
 
-        ],
-        function( declare,
-                  lang,
-                  _WidgetBase,
-                  _TemplatedMixin,
-                  _WidgetsInTemplateMixin,
-                  template,
-                  i18n
-            ) {
+], function(
+        declare,
+        lang,
+        _WidgetBase,
+        _TemplatedMixin,
+        _WidgetsInTemplateMixin,
+        template,
+        i18n
+    ) {
 
-            var FillStylePicker = declare( [ _WidgetBase,
-                _TemplatedMixin,
-                _WidgetsInTemplateMixin
-            ],
-            {
+    var FillStylePicker = declare( [
+        _WidgetBase,
+        _TemplatedMixin,
+        _WidgetsInTemplateMixin
+    ],
+    {
 
-                widgetsInTemplate: true,
-                templateString: template,
-                fillStyle: 'esriSFSSolid',
-                i18n: i18n,
-                label: 'Fill style:',
+        widgetsInTemplate: true,
+        templateString: template,
+        fillStyle: 'esriSFSSolid',
+        i18n: i18n,
+        label: 'Fill style:',
 
-                constructor: function( options ) {
+        constructor: function( options ) {
 
-                    options = options || {};
-                    lang.mixin( this, options );
+            options = options || {};
+            lang.mixin( this, options );
 
-                },
+        },
 
-                postCreate: function () {
+        postCreate: function () {
 
-                    this.inherited( arguments );
-                    this._set( 'fillStyle', this.fillStyle );
+            this.inherited( arguments );
+            this._set( 'fillStyle', this.fillStyle );
 
-                },
+        },
 
-                _setFillStyleAttr: function ( value ) {
+        _setFillStyleAttr: function ( value ) {
 
-                    this._updateSelectDijit( value );
-                    this.fillStyle = value;
+            this._updateSelectDijit( value );
+            this.fillStyle = value;
 
-                },
+        },
 
-                _updateSelectDijit: function ( value ) {
+        _updateSelectDijit: function ( value ) {
 
-                    if ( this.selectDijit ) {
-                        this.selectDijit.set( 'value', value );
-                    }
+            if ( this.selectDijit ) {
+                this.selectDijit.set( 'value', value );
+            }
 
-                },
+        },
 
-                _onSelectDijitChange: function( newValue ) {
+        _onSelectDijitChange: function( newValue ) {
 
-                    this._set( 'fillStyle', newValue );
-
-                }
-
-            } );
-
-            return FillStylePicker;
+            this._set( 'fillStyle', newValue );
 
         }
+
+    } );
+
+    return FillStylePicker;
+
+}
 
 );
